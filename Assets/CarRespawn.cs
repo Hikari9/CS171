2a3d4b2fe04b7d52cbc3706d260736541bd8e3ff
Rico Tiongson <thericotiongson@gmail.com>
2017-11-20 16:53:39 +0800

    Add respawn script for car

---
+++ Assets/CarRespawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRespawn : MonoBehaviour {
+
+    public Vector3 SpawnPoint;
+    public double TravelDistance;
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if ((this.transform.position - this.SpawnPoint).magnitude >= this.TravelDistance) {
+            this.transform.position = this.SpawnPoint;
+        }
+	}
+}
--- Assets/Standard Assets/Utility/WaypointProgressTracker.cs
+++ Assets/Standard Assets/Utility/WaypointProgressTracker.cs
@@ -92,24 +92,29 @@ namespace UnityStandardAssets.Utility
                     speed = Mathf.Lerp(speed, (lastPosition - transform.position).magnitude/Time.deltaTime,
                                        Time.deltaTime);
                 }
-                target.position =
-                    circuit.GetRoutePoint(progressDistance + lookAheadForTargetOffset + lookAheadForTargetFactor*speed)
-                           .position;
-                target.rotation =
-                    Quaternion.LookRotation(
-                        circuit.GetRoutePoint(progressDistance + lookAheadForSpeedOffset + lookAheadForSpeedFactor*speed)
-                               .direction);
 
-
-                // get our current progress along the route
-                progressPoint = circuit.GetRoutePoint(progressDistance);
-                Vector3 progressDelta = progressPoint.position - transform.position;
-                if (Vector3.Dot(progressDelta, progressPoint.direction) < 0)
+                if (circuit != null)
                 {
-                    progressDistance += progressDelta.magnitude*0.5f;
+                    target.position =
+                        circuit.GetRoutePoint(progressDistance + lookAheadForTargetOffset + lookAheadForTargetFactor * speed)
+                               .position;
+                    target.rotation =
+                        Quaternion.LookRotation(
+                            circuit.GetRoutePoint(progressDistance + lookAheadForSpeedOffset + lookAheadForSpeedFactor * speed)
+                                   .direction);
+
+                    // get our current progress along the route
+                    progressPoint = circuit.GetRoutePoint(progressDistance);
+                    Vector3 progressDelta = progressPoint.position - transform.position;
+                    if (Vector3.Dot(progressDelta, progressPoint.direction) < 0)
+                    {
+                        progressDistance += progressDelta.magnitude*0.5f;
+                    }
                 }
 
+
                 lastPosition = transform.position;
+
             }
             else
             {
